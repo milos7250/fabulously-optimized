a410a40490e15d54ed07202320558197d314e0fa
milos7250 <milos.micik7@gmail.com>
2021-12-09 17:27:00 +0000

    3.0.2-alpha2.b

--- Packwiz/1.18/config/slightguimodifications/cts.groovy
+++ Packwiz/1.18/config/slightguimodifications/cts.groovy
@@ -5,7 +5,7 @@ mainMenu {
             x { it - 2 }
             y { it - 20 }
         }
-        text = literal("Fabulously Optimized 3.0.0-alpha.2a")
+        text = literal("Fabulously Optimized 3.0.0-alpha.2b")
         align = "right"
         color = 0xFFFFFF
         hoveredColor = 0x55FFFF
